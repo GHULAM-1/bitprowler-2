934d50a0422130103068a1c7add6d24932a819a8
GHULAM-1 <ilyasghulam35@gmail.com>
2023-10-08 17:14:17 +0500

    changes in packages

--- src/components/packages/packagesContent.tsx
+++ src/components/packages/packagesContent.tsx
@@ -5,10 +5,10 @@ export default function PackagesContent({
 }: any) {
     return <>
         <div className="flex flex-col justify-center items-center">
-            {packageData.servicesIncluded.map((item:any)=> {
+            {packageData.servicesIncluded.map((item:any , index:number)=> {
                 return (
                     
-                    <PackagesTableRow mainService = {item.mainService}  deliverables =  {item.Deliverables}></PackagesTableRow>
+                    <PackagesTableRow mainService = {item.mainService}  deliverables =  {item.Deliverables} index = {index}></PackagesTableRow>
                 )
             })}
            
