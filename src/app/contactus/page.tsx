b05a539c17248e7fe84b0c04aaddda9e9af84756
GHULAM-1 <ilyasghulam35@gmail.com>
2023-10-25 04:20:58 +0500

    solve contactus page error

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,14 +1,10 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import Contact from "@/components/Contact";
-import Customers from "@/components/Customers";
-export default function ContactPage(){
-    return <>
-            <div className = "mt-8">
-        
-        <Contact></Contact>
-        <Customers></Customers>
-         
-        </div>
+import ContactusSection from "../(components)/contactusSection/contactusSection";
+export default function ContactPage() {
+  return (
+    <>
+      <div className="mt-8">
+        <ContactusSection></ContactusSection>
+      </div>
     </>
+  );
 }
