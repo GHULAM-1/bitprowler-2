f5e0ad0c39de0924ed8acffcf130e5e2535cce35
Muhammad <mgreat1337@gmail.com>
2023-11-07 02:15:59 +0500

    fix

--- src/app/services/data/servicesPageWebContent.ts
+++ src/app/services/data/servicesPageWebContent.ts
@@ -1,7 +1,7 @@
 export const servicesPageWebContent = {
   name: "Web Dev",
   hero: {
-    quote: "Web development is about doing more with less",
+    quote: "\"Effective web development is about doing more with less\"",
     author: "- BitProwler Technologies Pvt Ltd",
   },
 
