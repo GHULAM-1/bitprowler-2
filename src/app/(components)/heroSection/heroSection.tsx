3b4ead4f45fdbf611d50c26fe51e3c4a6148ca08
GHULAM-1 <ilyasghulam35@gmail.com>
2023-11-07 00:52:18 +0500

    test5

--- src/app/(components)/heroSection/heroSection.tsx
+++ src/app/(components)/heroSection/heroSection.tsx
@@ -25,11 +25,11 @@ export default function HeroSection() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center px-[1.86%] ">
       <div className="  top-div  flex flex-col justify-center items-center h-full w-full TABLET:px-[5%] TABLET:items-start SMALL_LAPTOP:items-center mt-[6.15%]">
-        <h1 className="text-Mobile-Head  font-medium  flex text-center TABLET:text-Tablet-S-Heading TABLET:text-left SMALL_LAPTOP:text-SMALL_LAPTOP-Subheading SMALL_LAPTOP:text-center dark:text-black text-white">
+        <h1 className="text-Mobile-Head  font-medium  flex text-center TABLET:text-Tablet-S-Heading TABLET:text-left SMALL_LAPTOP:text-SMALL_LAPTOP-Subheading SMALL_LAPTOP:text-center dark:text-white text-black">
           Revolutionizing Digital Realities,
         </h1>
 
-        <h1 className="text-Mobile-Head font-medium  text-center TABLET:text-Tablet-S-Heading TABLET:text-left SMALL_LAPTOP:text-SMALL_LAPTOP-Subheading SMALL_LAPTOP:text-center">
+        <h1 className="text-Mobile-Head font-medium  text-center TABLET:text-Tablet-S-Heading TABLET:text-left SMALL_LAPTOP:text-SMALL_LAPTOP-Subheading SMALL_LAPTOP:text-center dark:text-white text-black">
           One <span className="text-themeColor">Bit</span> at a Time.
         </h1>
         <Link href="/contactus">
