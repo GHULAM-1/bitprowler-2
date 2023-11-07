c582fecfc7814a326fd9fb4094f95b124dac4529
Muhammad <mgreat1337@gmail.com>
2023-11-07 08:31:46 +0500

    fixed typo

--- src/app/services/components/servicesPageDeliverables.tsx
+++ src/app/services/components/servicesPageDeliverables.tsx
@@ -5,7 +5,7 @@ export default function ServicesPageDeliverables({ deliverablesData }: any) {
       <div className=" px-[4.5rem] flex  justify-center items-center mt-[18rem] w-screen ">
         <div className="flex bg-background w-full flex-col flex-wrap justify-start items-start">
           <div className="TABLET:text-Tablet-Head SMALL_LAPTOP:text-SMALL_LAPTOP-Heading mb-[2rem] ">
-            Delivrables
+            Deliverables
           </div>
 
           <div className="bg-background flex SMALL_LAPTOP:w-[70%] TABLET:w-full flex-wrap justify-start items-center  ">
