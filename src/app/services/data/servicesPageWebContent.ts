03a4432662e1942129532f8f2917dcce7e9c962e
Muhammad <mgreat1337@gmail.com>
2023-11-07 12:59:03 +0500

    author removed on web dev

--- src/app/services/data/servicesPageWebContent.ts
+++ src/app/services/data/servicesPageWebContent.ts
@@ -2,7 +2,7 @@ export const servicesPageWebContent = {
   name: "Web Dev",
   hero: {
     quote: '"Effective web development is about doing more with less"',
-    author: "- BitProwler Technologies Pvt Ltd",
+    author: "",
   },
 
   whySection: {
