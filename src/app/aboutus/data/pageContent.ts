61da3598675d1d3e185c45661e99c041978eb781
Muhammad <mgreat1337@gmail.com>
2023-11-07 08:57:44 +0500

    about us content

--- src/app/aboutus/data/pageContent.ts
+++ src/app/aboutus/data/pageContent.ts
@@ -3,30 +3,30 @@ export const pageContent = {
     "Bitprowler is a full-service agency dedicated to crafting distinctive digital landscapes that provide you with a competitive edge.",
 
   manifesto:
-    "Lorem ipsum dolor sit amet consectetur. Elit massa malesuada sed sollicitudin. In at id arcu dolor quis. Erat sit mollis id vestibulum vel pellentesque porttitor mattis.",
+    "You focus on your dream; we'll take care of the rest.",
 
   stages: [
     {
       id: 1,
-      heading: "Brainstorm",
+      heading: "Industry Expertise",
       brief:
-        "Lorem ipsum dolor sit amet consectetur. Ligula velit viverra vitae mi elit pharetra iaculis mattis.",
+        "Our team's global experience and presence in industry Halls of Fame (Google, Microsoft, Apple, Twitter) showcase our deep industry knowledge and international reach.",
       number: "01",
     },
 
     {
       id: 2,
-      heading: "Brainstorm",
+      heading: "Innovation and Creativity",
       brief:
-        "Lorem ipsum dolor sit amet consectetur. Ligula velit viverra vitae mi elit pharetra iaculis mattis.",
+        "We are not bound by conventions; we redefine them. Our team's fresh ideas and creative solutions breathe life into every project, setting you apart in the digital landscape. Whether it's web development, design, or security, we inject innovation into every facet of our work.",
       number: "02",
     },
 
     {
       id: 3,
-      heading: "Brainstorm",
+      heading: "Flexible Services",
       brief:
-        "Lorem ipsum dolor sit amet consectetur. Ligula velit viverra vitae mi elit pharetra iaculis mattis.",
+        "We understand that one size doesn't fit all. That's why we offer not only individual services but also flexible bundled packages, such as Bit, Megabit, and Gigabit. These packages are designed to empower organizations at different scales, catering to their unique needs. Whether you require individual solutions or a comprehensive package, our goal is to provide the flexibility to meet your digital requirements efficiently and effectively.",
       number: "03 ",
     },
   ],
