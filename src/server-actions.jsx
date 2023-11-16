d286aa4d9225df1430b7b64481694fd11ed4e3b5
GHULAM-1 <ilyasghulam35@gmail.com>
2023-11-17 00:49:10 +0500

    create env file

--- src/server-actions.jsx
+++ src/server-actions.jsx
@@ -1,11 +1,9 @@
 "use server";
 import { Resend } from "resend";
-const resend = new Resend("re_WfRoBWzc_91FbtiMMQgAjTUW96F6snPkG");
-// import { EmailTemplate } from "./components/email-template";
+const resend = new Resend(process.env.RESEND_CONNECTION_STRING);
 import Email from "./components/email-template";
 export async function formSubmission(currentState, formData) {
   const email = formData.get("email");
-
   console.log(email);
   console.log(typeof email);
   try {
@@ -14,7 +12,6 @@ export async function formSubmission(currentState, formData) {
       to: [email],
       subject: "Thank you for contacting",
       react: <Email name={formData.get("name")} />,
-      
     });
 
     return { message: "success" };
