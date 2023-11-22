87e29ea6f3236e0f882090d57311ee9e366e4ede
GHULAM-1 <ilyasghulam35@gmail.com>
2023-11-23 02:14:08 +0500

    studio done

--- sanity/post.js
+++ sanity/post.js
@@ -9,14 +9,84 @@ const post = {
       type: "string",
     },
     {
+      title: "Read Next Articles",
+      name: "readNextArticles",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+
+    {
+      title: "Category",
+      name: "category",
+      type: "array",
+      of: [{ type: "string" }],
+      options: {
+        list: [
+          { title: "Web Dev", value: "web dev" },
+          { title: "App Dev", value: "app dev" },
+          { title: "Marketing", value: "marketing" },
+          { title: "Seo", value: "seo" },
+          { title: "Security", value: "security" },
+          { title: "UI/UX", value: "ui/ux" },
+          { title: "General", value: "general" },
+        ],
+      },
+    },
+
+    {
+      name: "bannerImage",
+      title: "Banner image",
+      type: "image",
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+      fields: [
+        {
+          name: "caption",
+          type: "string",
+          title: "Caption",
+        },
+      ],
+    },
+
+    {
+      name: "authorName",
+      title: "Author name",
+      type: "string",
+    },
+    {
+      name: "authorImage",
+      title: "Author image",
+      type: "image",
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+    },
+    {
+      title: "Author's linkedin",
+      name: "authorLinkedin",
+      type: "string",
+    },
+    {
+      title: "Author's twitter",
+      name: "authorTwitter",
+      type: "string",
+    },
+    {
       name: "slug",
       title: "Slug",
       type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
     },
     {
       name: "publishedAt",
       title: "Published at",
-      type: "datetime",
+      type: "datetime", 
     },
     {
       name: "body",
@@ -26,6 +96,7 @@ const post = {
         {
           type: "block",
         },
+        { type: "image" },
       ],
     },
   ],
