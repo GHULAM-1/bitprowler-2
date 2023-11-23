9568c246ee0e12c2e94475b2f59019bd51997bdd
GHULAM-1 <ilyasghulam35@gmail.com>
2023-11-23 12:59:01 +0500

    ok

---
+++ src/app/teamauth/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useTogglingStore } from "@/store/store";
+// import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+
+export default function Teamauth() {
+  const inputRef = useRef(null);
+  const router = useRouter();
+  const changeMemberStatus = useTogglingStore(
+    (state) => state.changeMemberStatus
+  );
+  const isTeamMember = useTogglingStore((state) => state.isTeamMember);
+
+  const handleClick = () => {
+    const currentPassword = inputRef.current.value;
+
+    console.log(isTeamMember);
+    console.log("team main ho");
+    if (currentPassword === "donkey") {
+      changeMemberStatus();
+      router.push("/studio");
+    } else {
+      alert("password not correct try again!!! ");
+    }
+  };
+
+  return (
+    <>
+      <div className="w-screen gap-[2rem] h-screen  flex flex-col justify-center items-center ">
+        <div className="flex justify-center items-center flex-col gap-[2rem] ">
+          <div className="font-medium text-Mobile-Text TABLET:text-Tablet-Large-Text ">
+            BitProwler <span className="text-themeColor">Admi</span>{" "}
+            Authorization{" "}
+          </div>
+          <div className="flex TABLET:flex-row flex-col gap-[1rem] items-center TABLET:px-[1rem]">
+            <label
+              htmlFor="password"
+              className="font-medium TABLET:text-Tablet-Text"
+            >
+              Password :{" "}
+            </label>
+            <input
+              type="password"
+              ref={inputRef}
+              name="password"
+              className="py-[1rem] bg-transparent outline-none  font-extrabold TABLET:text-Tablet-Large-Text text-[1rem] border-b-[1px] border-b-gray-800 "
+            />
+          </div>
+          <button
+            onClick={handleClick}
+            className="font-medium bg-themeColor px-[2rem] py-[1rem]  rounded-[1rem]"
+          >
+            submit
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
