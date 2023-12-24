c5b357f43880e1ef6ab16650793ec7d47f8e0426
GHULAM-1 <ilyasghulam35@gmail.com>
2023-12-24 17:24:13 +0500

    cleaning in progress

---
+++ src/data/aboutus/teamContent.ts
@@ -0,0 +1,65 @@
+export const teamContent = [
+  {
+    name: "Ghulam",
+    imagePath: "/teamMembers/ghulam.png",
+    socials: [
+      {
+        name: "linkedIn",
+        link: "linkedin.com",
+      },
+
+      {
+        name: "github",
+        link: "github.com",
+      },
+    ],
+  },
+
+  {
+    name: "Sarim",
+    imagePath: "/teamMembers/sarim.png",
+    socials: [
+      {
+        name: "linkedIn",
+        link: "linkedin.com",
+      },
+
+      {
+        name: "github",
+        link: "github.com",
+      },
+    ],
+  },
+
+  {
+    name: "Moiz",
+    imagePath: "/teamMembers/moiz.png",
+    socials: [
+      {
+        name: "linkedIn",
+        link: "linkedin.com",
+      },
+
+      {
+        name: "github",
+        link: "github.com",
+      },
+    ],
+  },
+
+  {
+    name: "Muhammad",
+    imagePath: "/teamMembers/muhammad.png",
+    socials: [
+      {
+        name: "linkedIn",
+        link: "linkedin.com",
+      },
+
+      {
+        name: "github",
+        link: "github.com",
+      },
+    ],
+  },
+];
