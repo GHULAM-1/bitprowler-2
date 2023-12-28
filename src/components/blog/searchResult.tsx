e66a81b286ed9822658f280f41efae4c0411db1f
GHULAM-1 <ilyasghulam35@gmail.com>
2023-12-28 21:50:31 +0500

    some minor changes

--- src/components/blog/searchResult.tsx
+++ src/components/blog/searchResult.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useTogglingStore } from "@/stores/togglingStore";
 import Link from "next/link";
 type SearchResultProps = {
   slug: string;
@@ -5,12 +7,23 @@ type SearchResultProps = {
   category?: string;
 };
 export default function SearchResult({ slug, mainHeading }: SearchResultProps) {
-  console.log("in search result", mainHeading);
+  const isSearchActive = useTogglingStore((state: any) => state.isSearchActive);
+
+  const toggleSearchWindow = useTogglingStore(
+    (state: any) => state.toggleSearchWindow
+  );
+  const handleClick = () => {
+    document.body.style.overflow = "visible";
+    if (isSearchActive) {
+      toggleSearchWindow();
+    }
+  };
   return (
     <>
       <Link
         href={`/blog/${slug}`}
         className="flex text-Mobile-Text TABLET:text-SMALL_LAPTOP-Text py-4 TABLET:py-6 hover:bg-[#cc2c1433]  transition-all ease-linear  px-4"
+        onClick={handleClick}
       >
         {mainHeading}
       </Link>
