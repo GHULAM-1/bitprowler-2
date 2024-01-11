190d216f66cb8d6d37297aae4357b2c54aff0d89
GHULAM-1 <ilyasghulam35@gmail.com>
2024-01-11 08:25:58 +0500

    added functionality of receiving the email notification

--- src/serverActions/server-actions.jsx
+++ src/serverActions/server-actions.jsx
@@ -4,10 +4,16 @@ const resend = new Resend(process.env.RESEND_CONNECTION_STRING);
 import Email from "../components/home/email-template";
 export async function formSubmission(currentState, formData) {
   const email = formData.get("email");
-  console.log(email);
-  console.log(typeof email);
+  const message = formData.get("message");
   try {
-    const data = await resend.emails.send({
+    await resend.emails.send({
+      from: "bitprowler <team@bitprowler.com>",
+      to: ["ilyasghulam35@gmail.com"], // Update with your email address
+      subject: "Guys We Have A Client",
+      text: message, // Include the message in the email body
+    });
+
+    await resend.emails.send({
       from: "bitprowler <team@bitprowler.com>",
       to: [email],
       subject: "Thank you for contacting",
